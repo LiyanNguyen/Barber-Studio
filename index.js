c9b5e7975bd5971a2bbce1bba9d38b84c5e2d038
LiyanNguyen <50958126+LiyanNguyen@users.noreply.github.com>
2022-02-24 15:20:23 +0800

    Update
    - added icon logo
    - fix some font size on services section on different viewports
    - moved media query into separate files instead for performance

--- index.js
+++ index.js
@@ -13,3 +13,21 @@ menuBtn.onclick = () => {
 		menu.src = "Images/menu.png";
 	}
 }
+
+window.onscroll = () => {
+	if (sideNav.classList.contains('openNav')) {
+		sideNav.classList.remove('openNav');
+		menu.src = "Images/menu.png";
+	}
+}
+
+//======= FUNCTION TO WAIT FOR ENTIRE FILES TO PARSE FIRST =======//
+let checkIfDocumentFullyLoaded = () => {
+	if (document.readyState === "complete") {
+		// make the entire page visible
+		document.querySelector('body').removeAttribute('style');
+		// remove the interval call to this function
+		clearInterval(checkPageLoad);
+	}
+};
+let checkPageLoad = setInterval(checkIfDocumentFullyLoaded, 100);
